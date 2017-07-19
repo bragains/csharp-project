91a804731a8c9d07a164d7d27eaa15ac213cf8eb
brittany ragains <ragainsdesigns@gmail.com>
2017-07-18 21:19:25 -0400

    Delete function added and working.

---
+++ My_project/Controllers/HomeController.cs
@@ -0,0 +1,115 @@
+﻿using My_project.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Dapper;
+using My_project.Repositories;
+using System.Net;
+
+namespace My_project.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Page currently being developed. Please check back soon.";
+
+            return View();
+        }
+
+        public ActionResult Invest()
+        {
+            ViewBag.Message = "Page currently being developed. Please check back soon.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Thanks for your interest in Kimberley Mining. We look forward to hearing from you.";
+
+            return View();
+        }
+
+        // CREATE USER
+
+        public ActionResult CreateUser()
+        {
+            return View("AddUser");
+        }
+
+
+        // ADD USER
+
+        public ActionResult AddUser(User user)
+        {
+            using (var repo = new UserRepository())
+            {
+                repo.AddUser(user);
+                return View("SingleUser", user);
+            }     
+        }
+
+        // VIEW USERS
+
+        public ActionResult ViewUsers()
+        {
+            using (var repo = new UserRepository())
+            {
+                return View("UserList", repo.Users.ToList());
+            }
+        }
+
+        // EDIT USER
+
+        public ActionResult EditUser(int id)
+        {
+            using (var repo = new UserRepository())
+            {
+                return View("EditUser", repo.GetUser(id));
+            }
+        }
+
+        // UPDATE USER
+
+        public ActionResult UpdateUser(User user)
+        {
+            using (var repo = new UserRepository())
+            {
+                repo.EditUser(user);
+                return View("UserList", repo.Users.ToList());
+            }
+        }
+
+        // DELETE USER
+
+        public ActionResult RemoveUser(int id)
+        {
+            using (var repo = new UserRepository())
+            {
+                return View("DeleteUser", repo.RetrieveUser(id));
+            }
+        }
+
+        
+        public ActionResult DeleteUser(User user)
+        {
+            using (var repo = new UserRepository())
+            {
+               repo.RemoveUser(user);
+               return View("UserList", repo.Users.ToList());
+           }
+       }
+
+    }
+
+}
---
+++ My_project/Repositories/UserRepository.cs
@@ -0,0 +1,53 @@
+﻿using My_project.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace My_project.Repositories
+{
+    public class UserRepository : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public UserRepository() : base("UserRepository")
+        {
+
+        }
+
+        public void AddUser(User user)
+        {
+            Users.Add(user);
+            this.SaveChanges();     
+        }
+
+        public User GetUser(int id)
+        {
+            return (from user in Users
+             where user.ID == id
+             select user).First();
+        }
+
+        public void EditUser(User user)
+        {
+            this.Entry(user).State = EntityState.Modified;
+            this.SaveChanges();
+        }
+
+        public User RetrieveUser(int id)
+        {
+            return (from user in Users
+                    where user.ID == id
+                    select user).First();
+        }
+
+        public void RemoveUser(User user)
+        {
+            this.Entry(user).State = EntityState.Deleted;
+            this.SaveChanges();
+        }
+
+
+    }
+}
